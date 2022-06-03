9dc7415053cb9cfb89384d9fef3d380c8de8b15f
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2022-06-03 20:57:54 -0300

    ref3

--- components/lists/colaborators/ColaboratorsSmall.tsx
+++ components/lists/colaborators/ColaboratorsSmall.tsx
@@ -1,5 +1,20 @@
 import { useState } from "react";
-import { Title } from "./styled_components";
+import {
+  Avatar,
+  Accordion,
+  Title,
+  Main,
+  NameTitle,
+  Description,
+  Name,
+  Cards,
+  Card,
+  CardTitle,
+  CardDesc,
+  BtnAction,
+  BtnLoad,
+  ContentShow,
+} from "./styled_components";
 
 export const ColaboratorsSmall = ({ rows }) => {
   const [selected, setSelected] = useState(null);
@@ -13,63 +28,53 @@ export const ColaboratorsSmall = ({ rows }) => {
   };
 
   return (
-    <div>
-      <div className="sc_wrapper">
-        <div className="sc_accordion">
-          {rows.slice(0, qtnColaborators).map((item, i) => (
-            <div key={item.id} className="sc_item" onClick={() => toogle(i)}>
-              <Title>
-                <div className="sc_main">
-                  <div className="sc_name_title">Nome Completo</div>
-                  <div className="sc_desc">
-                    <div className="sc_image">
-                      <img
-                        style={{ borderRadius: "50%" }}
-                        src="https://picsum.photos/30/30"
-                      />
-                    </div>
-                    <div className="sc_name">{item.name}</div>
-                  </div>
-                </div>
-                <span>{selected === i ? "-" : "+"}</span>
-              </Title>
-              <div className={selected === i ? "sc_show" : "sc_content"}>
-                <div className="sc_cards">
-                  <div className="sc_card">
-                    <div>Departamento</div>
-                    <div>{item.department}</div>
-                  </div>
-                  <div className="sc_card">
-                    <div>Cargo</div>
-                    <div>{item.role}</div>
-                  </div>
-                  <div className="sc_card">
-                    <div>Unidade</div>
-                    <div>{item.agent_id}</div>
-                  </div>
-                  <div className="sc_card">
-                    <div>Unidade</div>
-                    <div>{item.branch}</div>
-                  </div>
-                  <div className="sc_card">
-                    <div>Status</div>
-                    <div>{item.status}</div>
-                  </div>
-                </div>
-                <button>Ações</button>
-              </div>
-            </div>
-          ))}
-          <button
-            onClick={() => {
-              setQtnColaborators((range) => range + 5);
-            }}
-            className="sc_btn"
-          >
-            Carregar Mais
-          </button>
+    <Accordion>
+      {rows.slice(0, qtnColaborators).map((item, i) => (
+        <div key={item.id} className="sc_item" onClick={() => toogle(i)}>
+          <Title>
+            <Main>
+              <NameTitle>Nome Completo</NameTitle>
+              <Description>
+                <Avatar src="https://picsum.photos/200" />
+                <Name>{item.name}</Name>
+              </Description>
+            </Main>
+            <span>{selected === i ? "-" : "+"}</span>
+          </Title>
+          <ContentShow isSelected={selected === i}>
+            <Cards>
+              <Card>
+                <CardTitle>Departamento</CardTitle>
+                <CardDesc>{item.department}</CardDesc>
+              </Card>
+              <Card>
+                <CardTitle>Cargo</CardTitle>
+                <CardDesc>{item.role}</CardDesc>
+              </Card>
+              <Card>
+                <CardTitle>Unidade</CardTitle>
+                <CardDesc>{item.agent_id}</CardDesc>
+              </Card>
+              <Card>
+                <CardTitle>Unidade</CardTitle>
+                <CardDesc>{item.branch}</CardDesc>
+              </Card>
+              <Card>
+                <CardTitle>Status</CardTitle>
+                <CardDesc>{item.status}</CardDesc>
+              </Card>
+            </Cards>
+            <BtnAction>Ações</BtnAction>
+          </ContentShow>
         </div>
-      </div>
-    </div>
+      ))}
+      <BtnLoad
+        onClick={() => {
+          setQtnColaborators((range) => range + 5);
+        }}
+      >
+        Carregar Mais
+      </BtnLoad>
+    </Accordion>
   );
 };
--- components/lists/colaborators/styled_components.tsx
+++ components/lists/colaborators/styled_components.tsx
@@ -4,11 +4,15 @@ import { css } from "@emotion/react";
 export const Avatar = styled.img`
   color: turquoise;
   src: ${(props) => props.src};
-  alt: "";
+  alt: "avatar image";
   width: 32px;
   height: 32px;
   border-radius: 50%;
   margin-right: 12px;
+`;
+
+export const Accordion = styled.div`
+  width: 300px;
 `;
 
 export const Title = styled.div`
@@ -22,4 +26,59 @@ export const Title = styled.div`
 export const Main = styled.div`
   display: flex;
   flex-direction: column;
+`;
+
+export const NameTitle = styled.div`
+  color: red;
+`;
+
+export const Description = styled.div`
+  display: flex;
+`;
+
+export const Name = styled.div`
+  color: black;
+`;
+
+export const Cards = styled.div`
+  display: flex;
+  gap: 24px;
+  flex-wrap: wrap;
+`;
+
+export const Card = styled.div`
+  color: black;
+`;
+
+export const CardTitle = styled.div`
+  color: black;
+`;
+
+export const CardDesc = styled.div`
+  color: black;
+`;
+
+export const BtnAction = styled.button`
+  color: black;
+`;
+
+export const BtnLoad = styled.button`
+  width: 100%;
+  box-shadow: 0px 4px 8px rgba(165, 171, 179, 0.16);
+  border-radius: 8px;
+`;
+
+export const ContentShow = styled.div`
+  color: #8b7f75;
+  max-height: 0;
+  overflow: hidden;
+  transition: all 0.5s cubic-bezier(0, 1, 0, 1);
+
+  ${({ isSelected }) =>
+    isSelected &&
+    css`
+      height: auto;
+      max-height: 9999px;
+      transition: all 0.5s cubic-bezier(1, 0, 1, 0);
+    `};
 `;
